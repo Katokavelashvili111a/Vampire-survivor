da0380ae77202be09bdb0cd15669bc8a993a4595
Katokavelashvili111a <katokavela@gmail.com>
2026-06-14 19:07:13 +0200

    it was the commas.

--- code/sprites.py
+++ code/sprites.py
@@ -1,6 +1,6 @@
 from settings import *
 
-class CollisionSprite(pygame, sprite, Sprite):
+class CollisionSprite(pygame.sprite.Sprite):
     def __init__(self, pos, size, groups):
         super().__init__(groups)
         self.image = pygame.Surface(size)
