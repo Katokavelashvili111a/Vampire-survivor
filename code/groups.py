d9ccb16579167ea1cda4c54f91fe0b927884f611
Katokavelashvili111a <katokavela@gmail.com>
2026-06-15 00:49:21 +0200

    errorr fixed (misspelled pygame)

--- code/groups.py
+++ code/groups.py
@@ -1,5 +1,9 @@
 from settings import *
 
-class AllSprites(pygae.sprite.Group):
+class AllSprites(pygame.sprite.Group):
     def __init__(self):
         super().__init__()
+
+    def draw(self, surface):
+        for sprite in self:
+            surface.blit(sprite.image, sprite.rect)
