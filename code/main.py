8e8ff5142dd3f5cd2c89f56e7480aecef6aba480
Katokavelashvili111a <katokavela@gmail.com>
2026-06-15 00:40:37 +0200

    collisions

--- code/main.py
+++ code/main.py
@@ -31,6 +31,8 @@ class Game:
         for obj in map.get_layer_by_name('Objects'):
             CollisionSprite((obj.x, obj.y), obj.image, (self.all_sprites, self.collision_sprites))
 
+        for obj in map.get_layer_by_name('Collisions'):
+            CollisionSprite((obj.x, obj.y), pygame.Surface((obj.width, obj.height)), self.collision_sprites)
 
     def run(self):
         while self.running:
