12ad64d24dafd806361c31bea914614a5daaff96
Katokavelashvili111a <katokavela@gmail.com>
2026-06-14 15:43:38 +0200

    setting up. creating all the folders needed

---
+++ code/settings.py
@@ -0,0 +1,6 @@
+import pygame
+from oss.path import join
+from os import walk
+
+WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
+TILE_SIZE = 64
--- main.py
+++
@@ -1,8 +0,0 @@
-def main():
-    print("Hello from vampire-survivor-game!")
-
-
-if __name__ == "__main__":
-    main()
-
-test
