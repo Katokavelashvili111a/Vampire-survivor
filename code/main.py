0fc6e154c79c05e324aff12a3ab46841a30644e8
Katokavelashvili111a <katokavela@gmail.com>
2026-06-14 17:04:31 +0200

    little dude exists now

--- code/main.py
+++ code/main.py
@@ -1,12 +1,19 @@
 from settings import *
-
+from player import Player
 class Game:
     def __init__(self):
+        #setup
         pygame.init()
         self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
         pygame.display.set_caption('Survivor')
         self.clock = pygame.time.Clock()
         self.running = True
+
+        #groups? <-- figure out what this means!!
+        self.all_sprites = pygame.sprite.Group()
+        #these are the parameters that player is fetching
+        #sprites
+        self.player = Player((400, 300), self.all_sprites)
     
     def run(self):
         while self.running:
@@ -19,8 +26,10 @@ class Game:
                     self.running = False
             
             #update
-
+            #this will make the sprites update with delta time so the time that passes between every frame will be very short
+            self.all_sprites.update(dt)
             #draw
+            self.all_sprites.draw(self.display_surface)
             pygame.display.update()
         pygame.quit()
 
--- code/player.py
+++ code/player.py
@@ -3,4 +3,6 @@ from settings import *
 class Player(pygame.sprite.Sprite):
     def __init__(self, pos, groups):
         super().__init__(groups)
-        self.image = pygame.image.load 
+        self.image = pygame.image.load(join('images', 'player', 'down', '0.png')).convert_alpha()
+        self.rect = self.image.get_frect(center = pos)
+        #pos = position (will be fetched from parameter)
