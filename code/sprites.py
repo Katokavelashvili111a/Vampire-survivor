352d1074da0f2a44accd54bbdcbb9117a90727ba
Katokavelashvili111a <katokavela@gmail.com>
2026-06-15 03:12:23 +0200

    added gun class to sprites class and changed the image for gun

--- code/sprites.py
+++ code/sprites.py
@@ -6,8 +6,24 @@ class Sprite(pygame.sprite.Sprite):
         self.image = surf
         self. rect = self.image.get_frect(topleft = pos)
         self.ground = True
+
+
 class CollisionSprite(pygame.sprite.Sprite):
     def __init__(self, pos, surf, groups):
         super().__init__(groups)
         self.image = surf
         self. rect = self.image.get_frect(topleft = pos)
+
+
+class Gun(pygame.sprite.Sprite):
+    def __init__(self, player, groups):
+        #player connection
+        self.player = player
+        self.distance = 140
+        self.player_direction = pygame.Vector2(1,0)
+
+        #sprite setup
+        super().__init__(groups)
+        self.gun_surf = pygame.image.load(join('images', 'gun', 'gun.png'))
+        self.image
+        self.rect
